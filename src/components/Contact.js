c01ca850a574168c428801a189e6100fb31080ee
changshuyii <sueeloveblack@gmail.com>
2022-06-19 21:20:20 +0800

    feat(資料串接後的初步畫面): 資料串接後的初步畫面
    BREAKING CHANGE: no
    
    Closes no

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import moment from 'moment';
 import axios from 'axios';
 import {
   Card,
@@ -10,6 +11,103 @@ import {
 
 const Contact = () => {
   const [locationReponse, setLocationReponse] = useState();
+
+  const weatherElement = (element) => {
+    return element?.map((elementItem, _) => {
+      return elementItem?.time?.map((timeItem, _) => {
+        const nowDateTime = moment(new Date(), 'YYYY-MM-DD HH:mm:ss');
+        const startDateTime = moment(
+          timeItem?.startTime,
+          'YYYY-MM-DD HH:mm:ss'
+        );
+        const endDateTime = moment(timeItem?.endTime, 'YYYY-MM-DD HH:mm:ss');
+        if (nowDateTime.isBetween(startDateTime, endDateTime)) {
+          if (elementItem?.elementName === 'Wx') {
+            return (
+              <CardBody className="text-center">
+                <Typography variant="h5" className="mb-2">
+                  {timeItem?.parameter?.parameterName}
+                </Typography>
+              </CardBody>
+            );
+          }
+          if (elementItem?.elementName === 'PoP') {
+            return (
+              <CardFooter
+                divider
+                className="flex items-center justify-between py-3"
+              >
+                <Typography variant="small">降雨機率</Typography>
+                <Typography variant="small">
+                  {timeItem?.parameter?.parameterName + '%'}
+                </Typography>
+                <Typography
+                  variant="small"
+                  color="grey"
+                  className="flex gap-1"
+                ></Typography>
+              </CardFooter>
+            );
+          }
+          if (elementItem?.elementName === 'MaxT') {
+            return (
+              <CardFooter
+                divider
+                className="flex items-center justify-between py-3"
+              >
+                <Typography variant="small">最高溫度</Typography>
+                <Typography variant="small">
+                  {timeItem?.parameter?.parameterName + '度'}
+                </Typography>
+                <Typography
+                  variant="small"
+                  color="grey"
+                  className="flex gap-1"
+                ></Typography>
+              </CardFooter>
+            );
+          }
+          if (elementItem?.elementName === 'MinT') {
+            return (
+              <CardFooter
+                divider
+                className="flex items-center justify-between py-3"
+              >
+                <Typography variant="small">最低溫度</Typography>
+                <Typography variant="small">
+                  {timeItem?.parameter?.parameterName + '度'}
+                </Typography>
+                <Typography
+                  variant="small"
+                  color="grey"
+                  className="flex gap-1"
+                ></Typography>
+              </CardFooter>
+            );
+          }
+          if (elementItem?.elementName === 'CI') {
+            return (
+              <CardFooter
+                divider
+                className="flex items-center justify-between py-3"
+              >
+                <Typography variant="small">舒適度</Typography>
+                <Typography variant="small">
+                  {timeItem?.parameter?.parameterName + '度'}
+                </Typography>
+                <Typography
+                  variant="small"
+                  color="grey"
+                  className="flex gap-1"
+                ></Typography>
+              </CardFooter>
+            );
+          }
+        }
+        return '';
+      });
+    });
+  };
 
   const GetApiResponse = () => {
     axios({
@@ -42,38 +140,34 @@ const Contact = () => {
         {locationReponse &&
           locationReponse?.map((item, index) => {
             return (
-              <Card className="w-96 mt-6">
-                <CardHeader color="blue" className="relative h-56">
-                  <img
-                    src="/img/blog.jpg"
-                    alt="img-blur-shadow"
-                    className="w-full h-full"
-                  />
-                </CardHeader>
-                <CardBody className="text-center">
-                  <Typography variant="h5" className="mb-2">
-                    {item?.locationName}
-                  </Typography>
-                  <Typography>
-                    The place is close to Barceloneta Beach and bus stop just 2
-                    min by walk and near to &quot;Naviglio&quot; where you can
-                    enjoy the main night life in Barcelona.
-                  </Typography>
-                </CardBody>
-                <CardFooter
-                  divider
-                  className="flex items-center justify-between py-3"
-                >
-                  <Typography variant="small">$899/night</Typography>
-                  <Typography
-                    variant="small"
-                    color="grey"
-                    className="flex gap-1"
+              // Wx(天氣現象)、MaxT(最高溫度)、MinT(最低溫度)、CI(舒適度)、PoP(降雨機率)
+              <Card
+                className="w-96 m-10 py-6 px-3 font-mono"
+                key={'location_' + index}
+              >
+                <CardHeader className="relative h-56 text-5xl bg-grey-700 text-blue-grey-50 p-3 flex">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="h-8 w-8"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    stroke-width="2"
                   >
-                    <i className="fas fa-map-marker-alt fa-sm mt-[3px]" />
-                    Barcelona, Spain
-                  </Typography>
-                </CardFooter>
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"
+                    />
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"
+                    />
+                  </svg>
+                  {item?.locationName}
+                </CardHeader>
+                {weatherElement(item?.weatherElement)}
               </Card>
             );
           })}
